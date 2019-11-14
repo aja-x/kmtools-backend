eb80673943cb38fed3ae56e983384daf31f64ae9
aja-x <ajax.soft.en@gmail.com>
2019-11-14 11:51:06 +0700

    update relationship in Article model

--- app/Article.php
+++ app/Article.php
@@ -11,4 +11,24 @@ class Article extends Model
         'title', 'content', 'last_edited', 'published_date', 'id_user', 'id_interest_category', 'id_error_report',
     ];
 
+    public function test()
+    {
+        return $this->hasMany('App\Test');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function interestCategory()
+    {
+        return $this->belongsTo('App\InterestCategory');
+    }
+
+    public function errorReport()
+    {
+        return $this->belongsTo('App\ErrorReport');
+    }
+
 }
