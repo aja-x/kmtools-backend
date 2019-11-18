b955e546494ef08d13548d9f47d9ea35f0f35078
aja-x <ajax.soft.en@gmail.com>
2019-11-18 02:42:01 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ $router->group(['middleware' => 'auth'], function () use ($router) {
     $router->put('/troubleshoot/{id}/save', 'TroubleshootArticleController@save');
     $router->post('/troubleshoot/publish', 'TroubleshootArticleController@publish');
     $router->put('/troubleshoot/{id}/publish', 'TroubleshootArticleController@publish');
-    
+
     $router->post('/search[/{query}]', 'SearchDataController@result');
 
     $router->get('/article/{id}/comments', 'CommentController@getArticleComments');
