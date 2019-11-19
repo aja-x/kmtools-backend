f60366c60dd2e89266cead9256d7722b0635ae0f
VerentIrene <verent.flourencia.irene@gmail.com>
2019-11-19 12:42:49 +0700

    [add] Activity service for tracking history

---
+++ app/Services/ActivityService.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\ActivityHistory;
+use Illuminate\Support\Facades\Auth;
+
+class ActivityService
+{
+    public function updateFieldArticle($id)
+    {
+        $article = ActivityHistory::updateOrCreate(
+            ['id_user' => Auth::id(), 'id_article' => $id],
+            ['last_accessed' => date('Y-m-d H:i:s')]
+        );
+        return ($article) ? true : false;
+    }
+
+    public function updateFieldErrorReport($id)
+    {
+        $error_report = ActivityHistory::updateOrCreate(
+            ['id_user' => Auth::id(), 'id_error_report' => $id],
+            ['last_accessed' => date('Y-m-d H:i:s')]
+        );
+        return ($error_report) ? true : false;
+    }
+}
