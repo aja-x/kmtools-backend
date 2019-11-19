467176a633b69e0dc58769d9d7ae9e74ea313025
aja-x <ajax.soft.en@gmail.com>
2019-11-19 05:50:52 +0000

    Apply fixes from StyleCI

--- app/Services/ActivityService.php
+++ app/Services/ActivityService.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Services;
-
 
 use App\ActivityHistory;
 use Illuminate\Support\Facades\Auth;
@@ -15,6 +13,7 @@ class ActivityService
             ['id_user' => Auth::id(), 'id_article' => $id],
             ['last_accessed' => date('Y-m-d H:i:s')]
         );
+
         return ($article) ? true : false;
     }
 
@@ -24,6 +23,7 @@ class ActivityService
             ['id_user' => Auth::id(), 'id_error_report' => $id],
             ['last_accessed' => date('Y-m-d H:i:s')]
         );
+
         return ($error_report) ? true : false;
     }
 }
