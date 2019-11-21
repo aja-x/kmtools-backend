c156ca04fb1d0792fb4745a24656bd5baf373200
aja-x <ajax.soft.en@gmail.com>
2019-11-21 20:12:33 +0700

    [add] last_updated fillable field in error report

--- app/ErrorReport.php
+++ app/ErrorReport.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ErrorReport extends Model
 {
     protected $fillable = [
-        'title', 'content', 'id_user', 'id_interest_category',
+        'title', 'content', 'id_user', 'id_interest_category', 'last_updated',
     ];
 
     public function article()
