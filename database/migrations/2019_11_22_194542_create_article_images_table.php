7d7744890b2160a27338533175007aaf3300cb49
VerentIrene <verent.flourencia.irene@gmail.com>
2019-11-22 19:59:48 +0700

    [add] migration for article images table

---
+++ database/migrations/2019_11_22_194542_create_article_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_article')->unsigned()->index();
+            $table->bigInteger('id_image')->unsigned()->index();
+            $table->foreign('id_article')->references('id')
+                ->on('articles')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('id_image')->references('id')
+                ->on('images')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_images');
+    }
+}
