aacfdbecd9d104bbd9a5788a7cdd906a59ff6ac3
aja-x <ajax.soft.en@gmail.com>
2019-12-29 09:53:17 +0700

    [update] remove envsubst

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -4,10 +4,6 @@ set -e
 COMMIT_SHA1=$CIRCLE_SHA1
 
 export COMMIT_SHA1=$COMMIT_SHA1
-
-envsubst <./kube/wissen-backend-deployment.yml >./kube/wissen-backend-deployment.yml.out
-mv ./kube/wissen-backend-deployment.yml.out ./kube/wissen-backend-deployment.yml
-
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
 ./kubectl \
@@ -15,4 +11,4 @@ echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
   --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
-  apply -f ./kube/
+  apply -f ./deploy/k8s/
