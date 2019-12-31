e5ed61bb6a14a5474213b811da74322ff8bb3a21
aja-x <ajax.soft.en@gmail.com>
2019-12-31 12:07:11 +0700

    [update] auto deploy when updated image available

--- deploy/scripts/ci-deploy.sh
+++ deploy/scripts/ci-deploy.sh
@@ -6,6 +6,8 @@ COMMIT_SHA1=$CIRCLE_SHA1
 export COMMIT_SHA1=$COMMIT_SHA1
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
+sed -i 's|ajaxsoften/wissen-backend|ajaxsoften/wissen-backend:'"$CIRCLE_SHA1"'|' ./deploy/k8s/wissen-backend/wissen-backend-deployment.yml
+
 ./kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
