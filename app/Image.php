1a19281d02c7171b3c2eb8b11c91249521bf1dd6
VerentIrene <verent.flourencia.irene@gmail.com>
2019-12-31 18:06:30 +0700

    [update] Menghapus relasi ke Error Report

--- app/Image.php
+++ app/Image.php
@@ -16,10 +16,4 @@ class Image extends Model
             'id_article', 'id_image')->as('articleImages')->withTimestamps();
     }
 
-    public function errorReport()
-    {
-        return $this->belongsToMany('App\ErrorReport', 'error_report_images',
-            'id_error_report', 'id_image')
-            ->as('errorReportImages')->withTimestamps();
-    }
 }
