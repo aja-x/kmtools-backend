bef8bf38ab9e5bbb83849ab24c323d0bf59151d3
VerentIrene <verent.flourencia.irene@gmail.com>
2019-12-31 18:03:57 +0700

    [update] Menghapus relasi ke Error Report

--- database/migrations/2019_11_19_110727_create_activity_histories_table.php
+++ database/migrations/2019_11_19_110727_create_activity_histories_table.php
@@ -17,12 +17,9 @@ class CreateActivityHistoriesTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('id_user')->unsigned()->index();
             $table->bigInteger('id_article')->unsigned()->nullable()->index();
-            $table->bigInteger('id_error_report')->unsigned()->nullable()->index();
             $table->timestamp('last_accessed');
             $table->foreign('id_user')->references('id')
                 ->on('users')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('id_error_report')->references('id')
-                ->on('error_reports')->onUpdate('cascade')->onDelete('no action');
             $table->foreign('id_article')->references('id')
                 ->on('articles')->onUpdate('cascade')->onDelete('no action');
             $table->timestamps();
