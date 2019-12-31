121dbdc893ed8d29146d42777588cecd0b83c16a
VerentIrene <verent.flourencia.irene@gmail.com>
2019-12-31 18:07:57 +0700

    [update] Menghapus relasi ke Error Report

--- app/ActivityHistory.php
+++ app/ActivityHistory.php
@@ -7,17 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class ActivityHistory extends Model
 {
     protected $fillable = [
-        'id_user', 'id_article', 'id_error_report', 'last_accessed',
+        'id_user', 'id_article', 'last_accessed',
     ];
 
     public function article()
     {
         return $this->belongsTo('App\Article', 'id_article');
-    }
-
-    public function errorReport()
-    {
-        return $this->belongsTo('App\ErrorReport', 'id_error_report');
     }
 
     public function user()
