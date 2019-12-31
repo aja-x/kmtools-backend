a1326aa329537f5a0136c92fd61a1cc9521f0d6c
VerentIrene <verent.flourencia.irene@gmail.com>
2019-12-31 18:15:34 +0700

    [update] Menghapus relasi ke Error Report dan Test

--- app/Http/Controllers/SearchDataController.php
+++ app/Http/Controllers/SearchDataController.php
@@ -3,12 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-use App\ErrorReport;
 use App\Services\Http\Response;
-use App\Test;
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
 
 class SearchDataController extends Controller
 {
@@ -27,10 +24,8 @@ class SearchDataController extends Controller
 
         $resultArticle = Article::where('title', 'LIKE', "%{$request->input('query')}%")->get();
         $resultUser = User::where('name', 'LIKE', "%{$request->input('query')}%")->get();
-        $resultErrorReport = ErrorReport::where('title', 'LIKE', "%{$request->input('query')}%")->get();
 
         return Response::plain(['article' => ['count' => $resultArticle->count(), 'data' => $resultArticle],
-            'user' => ['count' => $resultUser->count(), 'data' => $resultUser],
-            'error_report' => ['count' => $resultErrorReport->count(), 'data' => $resultErrorReport], ]);
+            'user' => ['count' => $resultUser->count(), 'data' => $resultUser],]);
     }
 }
